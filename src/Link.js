da76e0be5282f89fe4cf4f50d68e2efe493a374b
tahany <tahanymohammed777@hotmail.com>
2023-02-18 21:57:59 +0200

    fix: styling

--- src/Link.js
+++ src/Link.js
@@ -1,11 +1,11 @@
 // import React, { useContext } from "react";
 // import NavigationContext from "./context/navigation";
 import useNavigation from "./hooks/use-navigation";
-import className from "classnames";
-function Link({ to, children }) {
+import classNames from "classnames";
+function Link({ to, children, className, activeClassName }) {
 //   const { navigate } = useContext(NavigationContext);
-const { navigate } = useNavigation();
-  const classes = className('text-blue-500');
+const { navigate, currentPath } = useNavigation();
+  const classes = classNames('text-blue-500', className, currentPath === to && activeClassName);
   const handleClick = (event) => {
     if(event.metaKey || event.ctrlKey){
         return;
