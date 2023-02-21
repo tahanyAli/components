712efe65d0aacdcdd69796bba0c9a10e305beb97
tahany777 <tahanymohammed777@hotmail.com>
2023-02-21 20:37:57 +0200

    feat: add dynamic table with sorting

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,14 +1,20 @@
 import React from "react";
-import Table from "../Table";
+// import Table from "../Table";
+import SortableTable from "../SortableTable";
 function TablePage() {
   const data = [
     { name: "Orange", color: "bg-orange-500", score: 5 },
     { name: "Apple", color: "bg-red-500", score: 3 },
     { name: "Banana", color: "bg-yellow-500", score: 1 },
     { name: "Lime", color: "bg-green-500", score: 4 },
+    { name: "Cherry", color: "bg-red-700", score: 2.5 },
   ];
   const config = [
-    { label: "Name", render: (fruit) => fruit.name },
+    {
+      label: "Name",
+      render: (fruit) => fruit.name,
+      sortValue: (fruit) => fruit.name,
+    },
     {
       label: "Color",
       render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
@@ -16,7 +22,13 @@ function TablePage() {
     {
       label: "Score",
       render: (fruit) => fruit.score,
-      header: () => <th className="bg-red-500">Score</th>,
+      sortValue: (fruit) => fruit.score,
+      // header: () => <th className="bg-red-500">Score</th>,
+    },
+    {
+      label: "Score Squared",
+      render: (fruit) => fruit.score ** 2,
+      sortValue: (fruit) => fruit.score ** 2,
     },
   ];
   const keyFn = (fruit) => {
@@ -24,7 +36,7 @@ function TablePage() {
   };
   return (
     <div>
-      <Table data={data} config={config} keyFn={keyFn} />
+      <SortableTable data={data} config={config} keyFn={keyFn} />
     </div>
   );
 }
