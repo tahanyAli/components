07cd432a27ca57d45e5f35464de2c76c3715cd8b
tahany777 <tahanymohammed777@hotmail.com>
2023-02-21 16:28:36 +0300

    fix table

--- src/pages/AccordionPage.js
+++ src/pages/AccordionPage.js
@@ -1,23 +1,23 @@
 import React from "react";
 
 function AccordionPage() {
-  const items = [
-    {
-      id: "12345",
-      label: "Can I use React on a project",
-      content: "You can use React on any project you want",
-    },
-    {
-      id: "6789",
-      label: "Can I use Javascript on a project",
-      content: "You can use Javascript on any project you want",
-    },
-    {
-      id: "34567",
-      label: "Can I use React on a project",
-      content: "You can use React on any project you want",
-    },
-  ];
+  // const items = [
+  //   {
+  //     id: "12345",
+  //     label: "Can I use React on a project",
+  //     content: "You can use React on any project you want",
+  //   },
+  //   {
+  //     id: "6789",
+  //     label: "Can I use Javascript on a project",
+  //     content: "You can use Javascript on any project you want",
+  //   },
+  //   {
+  //     id: "34567",
+  //     label: "Can I use React on a project",
+  //     content: "You can use React on any project you want",
+  //   },
+  // ];
   return <div>AccordionPage</div>;
 }
 
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -8,13 +8,20 @@ function TablePage() {
     { name: "Lime", color: "bg-green-500", score: 4 },
   ];
   const config = [
-    { label: 'Name', render: (fruit)=> fruit.name },
-    { label: 'Color', render: (fruit)=> <div className={`p-3 m-2 ${fruit.color}`}></div> },
-    { label: 'Score', render: (fruit)=>fruit.score },
+    { label: "Name", render: (fruit) => fruit.name },
+    {
+      label: "Color",
+      render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
+    },
+    {
+      label: "Score",
+      render: (fruit) => fruit.score,
+      header: () => <th className="bg-red-500">Score</th>,
+    },
   ];
   const keyFn = (fruit) => {
     return fruit.name;
-  }
+  };
   return (
     <div>
       <Table data={data} config={config} keyFn={keyFn} />
